ac5b84e0b94f53f682710cb74067947762466dd1
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-09-23 12:30:27 -0300

    Logica: Menu Hamburguesa

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+/*Declaraciones*/
+    const d = document
+    const menuBtn = d.querySelector('nav .btn')
+    const menuLst = d.querySelector('nav .menu')
+
+    menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
