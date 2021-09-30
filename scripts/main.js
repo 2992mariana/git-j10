05d91993f51e26a7ef637508ac4b829eaa74ed8b
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-09-30 13:04:31 -0300

    Componente: Mapa de Contacto

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,9 @@
     const d = document
     const menuBtn = d.querySelector('nav .btn')
     const menuLst = d.querySelector('nav .menu')
+    const mapBtn = d.querySelector('.map .btn')
+    const mapFrm = d.querySelector('.map iframe')
 
     menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
+
+    mapBtn.onclick = () => mapFrm.classList.toggle('active') ? mapBtn.classList.replace('icon-max', 'icon-min') : mapBtn.classList.replace('icon-min', 'icon-max') 
